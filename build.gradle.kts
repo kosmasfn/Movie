1abb1a737be64754bf291581b19c9dc5f9dc29e8
kosmas <kosmasfn@gmail.com>
2026-02-04 17:54:00 +0700

    add missing serialization dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     id("com.google.dagger.hilt.android") version "2.59" apply false
     id("com.google.devtools.ksp") version "2.3.5" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0" apply false
 }
